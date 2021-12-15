27f0116bdf0fb86137d4ebd08ed88bbdaca202e6
KimvanTartwijk <93664366+KimvanTartwijk@users.noreply.github.com>
2021-12-15 12:23:33 +0100

    Create script.js

---
+++ 12_Opdracht_Use_if-else_Statements/script.js
@@ -0,0 +1,33 @@
+let age = 30;
+
+if (age > 18) {
+
+    console.log("You are over 18, welcome!");
+
+} else {
+
+    console.log("You are not over 18, unfortunately you cannot enter.");
+}
+
+let isFemale = true;
+
+if (isFemale) {
+
+    console.log("Welcome to the ladies night!");
+
+} else {
+
+    console.log("Unfortunately, only ladies are welcome!");
+}
+
+let driverStatus = "bob";
+
+if (driverStatus == "bob") {
+
+    console.log("You are allowed to drive");
+
+} else {
+
+    console.log("You are not allowed to drive");
+
+}
