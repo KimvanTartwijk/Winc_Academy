d2b911d90a999ea58adc0db560adbbb7149e6ab0
KimvanTartwijk <93664366+KimvanTartwijk@users.noreply.github.com>
2021-12-15 01:57:08 +0100

    Add files via upload

---
+++ 10_Opdracht_Use_Your_First_Javascript/script.js
@@ -0,0 +1,31 @@
+console.log("Hello Winc Academy")
+
+let name = 'Kim';
+console.log(name)
+
+let sum = 4 + 4;
+console.log(sum)
+
+let sum2 = "4" + "4";
+console.log(sum2)
+
+let sum3 = 2 + 3;
+console.log(sum3)
+
+let sum4 = 6 - 1;
+console.log(sum4)
+
+let sum5 = 2 * 5;
+console.log(sum5)
+
+let sum6 = 8 / 2;
+console.log(sum6)
+
+let sum7 = 3 % 12;
+console.log(sum7)
+
+let age = 30;
+console.log(typeof 30)
+
+let age2 = 'thirty';
+console.log(typeof thirty)
