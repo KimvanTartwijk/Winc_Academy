a8c6d4b6a8308c148c3deb3495bbd33c5b23379d
KimvanTartwijk <93664366+KimvanTartwijk@users.noreply.github.com>
2021-12-15 11:34:43 +0100

    Create script.js

---
+++ 11_Opdracht_Run_a_JS_Program_on_your_computer/script.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy students")
+
+// This is a large sum
+/* let multiplication = 1230941 * 1823794;
+console.log("Multiplication:", multiplication)
+
+let division = 637263 / 54;
+console.log("Division:", division) */
