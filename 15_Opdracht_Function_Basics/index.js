df5d926d20043cb36cec8b607120e5ea771c494e
KimvanTartwijk <93664366+KimvanTartwijk@users.noreply.github.com>
2021-12-20 12:22:24 +0100

    Create index.js

---
+++ 15_Opdracht_Function_Basics/index.js
@@ -0,0 +1,38 @@
+// Opdracht 1
+
+const makePizza = function () {
+    console.log("Make dough for pizza bottom");
+    console.log("Roll dough out into a flat circle");
+    console.log("Put toppings on dough");
+    console.log("Bake in oven");
+};
+
+console.log("How to make pizza:")
+makePizza();
+
+// How to call this function three times:
+makePizza();
+makePizza();
+makePizza();
+
+// Opdracht 2
+
+const makeSushi = function () {
+    console.log("Cook sushi rice")
+    console.log("Spread a thin layer of rice on nori sheet");
+    console.log("Put ingredients on rice");
+    console.log("Make a roll");
+    console.log("Cut roll in pieces");
+}
+
+console.log("How to make sushi:");
+makeSushi();
+
+// Opdracht 3
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
