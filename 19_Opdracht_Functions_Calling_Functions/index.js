d58e0f6cdec062629d6b4004694b4e31c923998a
KimvanTartwijk <93664366+KimvanTartwijk@users.noreply.github.com>
2021-12-24 12:47:15 +0100

    Create index.js

---
+++ 19_Opdracht_Functions_Calling_Functions/index.js
@@ -0,0 +1,50 @@
+// Opdracht Hey Kiddo!
+
+const isAdult = function (age) {
+    console.log("Entering isAdult");
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+const greet = function (age) {
+    console.log("Entering greet");
+    if (isAdult(age)) {
+        return ("Hello there!");
+    } else {
+        return ("Hey kiddo!");
+    };
+}
+
+console.log(greet(17));
+
+// Opdracht VAT Calculations, exercise 1
+
+const taxPrice = function (basePrice, taxPercentage) {
+    const tax = (basePrice / 100) * taxPercentage;
+    return tax
+}
+
+const totalPrice = function (basePrice, taxPercentage) {
+    const total = basePrice + taxPrice(basePrice, taxPercentage);
+    return ["Total price:", total];
+}
+
+console.log(totalPrice(85, 21));
+
+// Opdracht VAT Calculations, exercise 2
+
+const basePrice = function (totalPrice, taxPercentage) {
+    const taxAmount = (totalPrice / 100) * taxPercentage;
+    const basePrice = totalPrice - taxAmount;
+    return basePrice;
+}
+
+const loosePrices = function (totalPrice, taxPercentage) {
+    const taxAmount = (totalPrice / 100) * taxPercentage;
+    return ['Price without tax:', basePrice(totalPrice, taxPercentage), "Price tax:", taxAmount];
+}
+
+console.log(loosePrices(53, 9));
